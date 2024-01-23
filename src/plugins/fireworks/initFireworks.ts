422dc016419561bdc177b648d4b2c21770fdd1c0
yixiaojiu <yi_xiao_jiu@qq.com>
2024-01-23 14:51:34 +0800

    fix: :bug: init firework effect once

--- src/plugins/fireworks/initFireworks.ts
+++ src/plugins/fireworks/initFireworks.ts
@@ -1,16 +1,25 @@
 import { createFireworks } from '@explosions/fireworks'
-import type { ClientModule } from '@docusaurus/types'
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
+// import type { ClientModule } from '@docusaurus/types'
 
-const module: ClientModule = {
-  onRouteDidUpdate() {
-    if (typeof window === 'undefined') return
-
-    createFireworks({
-      selector: 'canvas.fireworks',
-      // colors: ['#66A7DD', '#3E83E1', '#214EC2'],
-      colors: ['#FFC0D9', '#FFD1DA', '#F1B4BB'],
-    })
-  },
+if (ExecutionEnvironment.canUseDOM) {
+  createFireworks({
+    selector: 'canvas.fireworks',
+    // colors: ['#66A7DD', '#3E83E1', '#214EC2'],
+    colors: ['#FFC0D9', '#FFD1DA', '#F1B4BB'],
+  })
 }
 
-export default module
+// const module: ClientModule = {
+//   onRouteDidUpdate() {
+//     if (typeof window === 'undefined') return
+
+//     createFireworks({
+//       selector: 'canvas.fireworks',
+//       // colors: ['#66A7DD', '#3E83E1', '#214EC2'],
+//       colors: ['#FFC0D9', '#FFD1DA', '#F1B4BB'],
+//     })
+//   },
+// }
+
+// export default module
