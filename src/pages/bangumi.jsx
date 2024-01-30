4029d55170fa4f8c8038e8699c1a4e5de5cefb65
yixiaojiu <yi_xiao_jiu@qq.com>
2024-01-30 20:45:19 +0800

    feat: :sparkles: bangumi theme

--- src/pages/bangumi.jsx
+++
@@ -1,8 +0,0 @@
-import Layout from '@theme/Layout'
-import { defineCustomElements } from 'bilibili-bangumi-component/loader'
-
-defineCustomElements()
-
-export default function Bangumi() {
-  return <Layout></Layout>
-}
